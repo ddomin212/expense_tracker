14870a3460f4ee19f903f789ea51893077e3bf22
Daniel Dominko <danieldominko@gmail.com>
2023-11-10 21:58:06 +0100

    stuff

--- server/src/controllers/apis/currencyController.js
+++ server/src/controllers/apis/currencyController.js
@@ -1,25 +1,25 @@
-const User = require("../../models/User"); // Importing the User model
-const currencyData = require("../../../data/currency.json"); // Importing currency data from a JSON file
+const User = require("../../models/User");
+const currencyData = require("../../../data/currency.json");
 
 //@desc Update user currency
 //@route PATCH /api/users/currency
 //@access Private
 const updateUserCurrency = async (req, res) => {
-  const { currency } = req.body; // Extracting the currency value from the request body
-  const user = await User.findById(req.user._id); // Finding the user by ID
-  if (!user) return res.status(404).json({ message: "User not found" }); // If user is not found, return a 404 error
-  user.currency = currency; // Updating the user's currency value
-  await user.save(); // Saving the updated user object
-  res.status(200).json({ currency: user.currency }); // Returning the updated currency value
+  const { activeCurrency } = req.body;
+  const user = await User.findById(req.user._id);
+  if (!user) return res.status(404).json({ message: "User not found" });
+  user.currency = activeCurrency;
+  await user.save();
+  res.status(200).json({ currency: user.currency });
 };
 
 //@desc Get user currency
 //@route GET /api/users/currency
 //@access Private
 const getUserCurrency = async (req, res) => {
-  const user = await User.findById(req.user._id); // Finding the user by ID
-  if (!user) return res.status(404).json({ message: "User not found" }); // If user is not found, return a 404 error
-  res.status(200).json({ currency: user.currency, currencyData }); // Returning the user's currency value and the currency data
+  const user = await User.findById(req.user._id);
+  if (!user) return res.status(404).json({ message: "User not found" });
+  res.status(200).json({ currency: user.currency, currencyData });
 };
 
 module.exports = {
